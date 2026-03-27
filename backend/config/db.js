01358310b0a5ffdcb6cc0002346d9850cb9327e5
Esteban emanuel <estebanemanuel@192.168.1.153>
2026-03-27 10:57:04 -0300

    fix conexion final mysql railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,23 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+let db;
+
+if (process.env.DATABASE_URL) {
+  // 🔥 Railway (producción)
+  db = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  // 💻 Local (tu PC)
+  db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
 
 module.exports = db;
